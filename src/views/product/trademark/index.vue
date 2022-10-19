a0d029ddea0bc9152fc9c87ca330f66a511ac2ce
jiani <zxc@123.com>
2022-10-19 21:17:13 +0800

    表单验证

--- src/views/product/trademark/index.vue
+++ src/views/product/trademark/index.vue
@@ -1,6 +1,52 @@
 <template>
   <el-card>
-    <el-button type="primary" :icon="Plus">添加品牌</el-button>
+    <el-button type="primary" :icon="Plus" @click="showDialogFormVisible"
+      >添加品牌</el-button
+    >
+
+    <el-dialog v-model="dialogFormVisible" title="添加品牌">
+      <el-form
+        label-width="100px"
+        ref="ruleFormRef"
+        :model="trademarkFormData"
+        :rules="rules"
+      >
+        <el-form-item label="添加品牌" prop="tmName">
+          <el-input
+            v-model="trademarkFormData.tmName"
+            placeholder="请输入品牌名称"
+          />
+        </el-form-item>
+        <el-form-item label="品牌LOGO" prop="logoUrl">
+          <el-upload
+            class="avatar-uploader"
+            :action="`${BASE_URL}/admin/product/fileUpload`"
+            :show-file-list="false"
+            :on-success="handleAvatarSuccess"
+            :before-upload="beforeAvatarUpload"
+          >
+            <img
+              v-if="trademarkFormData.logoUrl"
+              :src="trademarkFormData.logoUrl"
+              class="avatar"
+            />
+            <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
+            <template #tip>
+              <div class="el-upload__tip">
+                只能上传jpg/png文件，且不超过50kb
+              </div>
+            </template>
+          </el-upload>
+        </el-form-item>
+      </el-form>
+      <template #footer>
+        <span class="dialog-footer">
+          <el-button @click="dialogFormVisible = false">取消</el-button>
+          <el-button type="primary">确认</el-button>
+        </span>
+      </template>
+    </el-dialog>
+
     <el-table
       :data="trademarkList"
       border
@@ -26,6 +72,7 @@
         </template>
       </el-table-column>
     </el-table>
+    <!-- 分页器 -->
     <div class="demo-pagination-block">
       <el-pagination
         class="paginationBorder"
@@ -49,19 +96,58 @@ export default {
 
 <script lang="ts" setup>
 import { Plus, EditPen, DeleteFilled } from "@element-plus/icons-vue";
-import { onMounted, ref } from "vue";
+import { onMounted, ref, reactive } from "vue";
 import { getTradeMarkApi } from "@/api/trademark";
+import { ElMessage } from "element-plus";
+import type { UploadProps, FormInstance, FormRules } from "element-plus";
+
+// 请求前缀
+const BASE_URL = import.meta.env.VITE_API_URL;
+
+/***校验规则****/
+//表单数据
+const trademarkFormData = reactive({
+  tmName: "",
+  // 品牌logo
+  logoUrl: "",
+});
+//表单实列
+const ruleFormRef = ref<FormInstance>();
+//验证规则
+const rules = reactive<FormRules>({
+  tmName: [{ required: true, message: "请输入上传名称", trigger: "blur" }],
+  logoUrl: [{ required: true, message: "请上传品牌LOGO" }],
+});
+
+/***添加品牌***/
+// const imageUrl = ref("");
+//上传成功
+const handleAvatarSuccess: UploadProps["onSuccess"] = (response) => {
+  trademarkFormData.logoUrl = response.data;
+  ruleFormRef.value?.clearValidate(["logoUrl"]);
+};
+
+// rawFile 上传的文件对象
+// rawFile.type 文件类型
+// rawFile.size 文件大小
+//上传之前
+const beforeAvatarUpload: UploadProps["beforeUpload"] = (rawFile) => {
+  const rawFileType = ["image/jpeg", "image/jpg", "image/png"];
+  if (!rawFileType.includes(rawFile.type)) {
+    ElMessage.error("图片只能上传jpeg,jpg和png类型的");
+    return false;
+  } else if (rawFile.size / 1024 > 250) {
+    ElMessage.error("图片大小必须小于250kb!");
+    return false;
+  }
+  return true;
+};
 //总页数
 const total = ref(500);
 //每条页数
 const pageSize = ref(5);
 //当前页
 const currentPage = ref(1);
-
-// 当page-size发送变化时触发的事件
-const handleSizeChange = () => {};
-// 当currentPage发送变化时触发的事件
-const handleCurrentChange = () => {};
 
 /****请求品牌数据***/
 const trademarkList = ref([]);
@@ -79,9 +165,19 @@ const getTrademarkList = async () => {
 onMounted(() => {
   getTrademarkList();
 });
+
+/***加载框****/
+const dialogFormVisible = ref(false);
+
+const showDialogFormVisible = () => {
+  dialogFormVisible.value = true;
+};
 </script>
 
 <style scoped lang="scss">
+.tableline {
+  margin: 20px 0;
+}
 .paginationBorder:deep() {
   .el-pagination__sizes {
     position: absolute;
@@ -92,11 +188,36 @@ onMounted(() => {
     right: 30px;
   }
 }
-.tableline {
-  margin: 20px 0;
-}
 .trademark-img {
   width: 200px;
   height: 100px;
+}
+.avatar-uploader .avatar {
+  width: 178px;
+  height: 178px;
+  display: block;
+}
+</style>
+
+<style>
+.avatar-uploader .el-upload {
+  border: 1px dashed var(--el-border-color);
+  border-radius: 6px;
+  cursor: pointer;
+  position: relative;
+  overflow: hidden;
+  transition: var(--el-transition-duration-fast);
+}
+
+.avatar-uploader .el-upload:hover {
+  border-color: var(--el-color-primary);
+}
+
+.el-icon.avatar-uploader-icon {
+  font-size: 28px;
+  color: #8c939d;
+  width: 178px;
+  height: 178px;
+  text-align: center;
 }
 </style>
