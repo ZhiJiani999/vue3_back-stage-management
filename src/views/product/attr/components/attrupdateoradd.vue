1aa8176a410b16411839a85c1eb12a2537a6c938
jiani <zxc@123.com>
2022-10-22 00:54:12 +0800

    自动获取焦点

--- src/views/product/attr/components/attrupdateoradd.vue
+++ src/views/product/attr/components/attrupdateoradd.vue
@@ -1,30 +1,37 @@
 <template>
-  <el-button @click="isShowList">取消</el-button>
   <el-card class="ma-20">
-    <el-form class="form-AttrData">
+    <el-form>
       <el-form-item label="属性名">
         <el-input placeholder="请输入属性名" v-model="formDataList.attrName" />
       </el-form-item>
       <el-from-item>
-        <el-button type="primary">添加属性值</el-button>
-        <el-button>取消</el-button>
+        <el-button
+          type="primary"
+          :icon="Plus"
+          :disabled="!formDataList.attrName"
+          @click="addAttrValue"
+          >添加属性值</el-button
+        >
+        <el-table :data="formDataList.attrValueList" style="width: 100%">
+          <el-table-column label="序号" width="60" type="index">
+          </el-table-column>
+          <el-table-column label="属性值名称" width="800">
+            <template v-slot="{ row }">
+              <el-input v-model="row.valueName" ref="inputRef" />
+            </template>
+          </el-table-column>
+          <el-table-column label="操作">
+            <!-- <template #reference> -->
+            <!-- <el-button size="small" type="danger" :icon="Delete"></el-button> -->
+            <el-button type="danger" :icon="Delete" size="small" />
+            <!-- </template> -->
+          </el-table-column>
+        </el-table>
       </el-from-item>
-      <el-table :data="formDataList.attrValueList" style="width: 100%">
-        <el-table-column label="序号" width="60" type="index">
-        </el-table-column>
-        <el-table-column label="属性值名称" width="500">
-          <template v-slot="{ row }">
-            <el-input v-model="row.valueName" />
-          </template>
-        </el-table-column>
-        <el-table-column label="操作">
-          <template>
-            <el-button size="small" type="danger" :icon="Delete"></el-button>
-          </template>
-        </el-table-column>
+      <el-form-item class="ma-10">
         <el-button type="primary">保存</el-button>
-        <el-button>取消</el-button>
-      </el-table>
+        <el-button @click="isShowList">取消</el-button>
+      </el-form-item>
     </el-form>
   </el-card>
 </template>
@@ -36,25 +43,25 @@ export default {
 </script>
 
 <script lang="ts" setup>
-import { Delete } from "@element-plus/icons-vue";
-import { ref } from "vue";
+import { Delete, Plus } from "@element-plus/icons-vue";
+import { ref, nextTick, reactive } from "vue";
+
 const emit = defineEmits(["setShowList"]);
 const isShowList = () => {
   emit("setShowList", true);
 };
-
-const formDataList = ref({
+const inputRef = ref();
+const formDataList = reactive({
   attrName: "",
-  attrValueList: [{}],
+  attrValueList: [],
 });
+
+const addAttrValue = () => {
+  formDataList.attrValueList.push({});
+  nextTick(() => {
+    inputRef.value.focus();
+  });
+};
 </script>
 
-<style scoped lang="sass">
-.form-AttrData{
-  .el-form-item{
-    .el-input{
-      width:fit-content
-    }
-  }
-}
-</style>
+<style scoped></style>
